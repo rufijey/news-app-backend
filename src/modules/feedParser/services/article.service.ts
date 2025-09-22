f929ba71321f4c41bb3e24ada417227d6c8cf1e4
Alexander <sanyatik@gmail.com>
2025-09-23 02:58:22 +0300

    fix

--- src/modules/feedParser/services/article.service.ts
+++ src/modules/feedParser/services/article.service.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import * as cheerio from "cheerio/dist/browser";
+import * as cheerio from "cheerio";
 import type { FastifyInstance } from "fastify";
 import type { Article } from "../types/news.types";
 
