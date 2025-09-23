1163ce80cd7f6e26a983c226fe92cc5fbd29baf0
Alexander <sanyatik@gmail.com>
2025-09-23 15:12:43 +0300

    schema fix

--- src/modules/feedParser/schemas/getArticleData.ts
+++ src/modules/feedParser/schemas/getArticleData.ts
@@ -16,17 +16,5 @@ export const getArticleDataSchema = {
             },
             additionalProperties: false,
         },
-        400: {
-            type: "object",
-            properties: {
-                error: { type: "string" },
-            },
-        },
-        500: {
-            type: "object",
-            properties: {
-                error: { type: "string" },
-            },
-        },
     },
 } as const;
