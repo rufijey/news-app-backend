f5c19e25befeea9c026201fca68d7425ebe5224b
Alexander <sanyatik@gmail.com>
2025-09-24 14:59:15 +0300

    fix

--- src/plugins/cors.plugin.ts
+++ src/plugins/cors.plugin.ts
@@ -5,7 +5,7 @@ import fp from "fastify-plugin";
 export default fp(
     async (fastify: FastifyInstance) => {
         await fastify.register(cors, {
-            origin: ["http://localhost:5174"],
+            origin: ["http://localhost:5173"],
             methods: ["GET", "POST", "PUT", "DELETE"],
             allowedHeaders: ["Content-Type", "Authorization"],
             credentials: true,
