a111a508b3cff850bf6fc8a4aedd2427c7160715
Alexander <sanyatik@gmail.com>
2025-09-24 13:29:06 +0300

    added cors

---
+++ src/plugins/cors.plugin.ts
@@ -0,0 +1,17 @@
+import cors from "@fastify/cors";
+import type { FastifyInstance } from "fastify";
+import fp from "fastify-plugin";
+
+export default fp(
+    async (fastify: FastifyInstance) => {
+        await fastify.register(cors, {
+            origin: ["http://localhost:5174"],
+            methods: ["GET", "POST", "PUT", "DELETE"],
+            allowedHeaders: ["Content-Type", "Authorization"],
+            credentials: true,
+        });
+
+        fastify.log.info("✅ CORS enabled");
+    },
+    { name: "cors-plugin" },
+);
